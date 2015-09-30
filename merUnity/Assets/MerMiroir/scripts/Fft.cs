f6ef67e8987d9360bff33f2aae1b19c08d71b8e8
soufyaneJanati <s2janati@enib.fr>
2015-09-30 08:18:58 +0200

    no message

---
+++ merUnity/Assets/MerMiroir/scripts/Fft.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fft : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ merUnity/Assets/MerMiroir/scripts/Graphs.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Graphs : MonoBehaviour {
+    public float val1;
+    public AnimationCurve courbe1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentTimer+=Time.deltaTime;
+        courbe1.AddKey(currentTimer, val1);
+	
+	}
+    private float currentTimer;
+}

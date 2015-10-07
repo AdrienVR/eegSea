8829b875b78b6aa6e1b492e1ac4b6a58cca49076
mathieu-quentel <mathieu.quentel@ymail.com>
2015-10-07 15:25:22 +0200

    Nettoyage et commentaires

--- pythonModule/AquisitionEEGclientUDPandOSC.py
+++ pythonModule/AquisitionEEGclientUDPandOSC.py
@@ -1,15 +1,5 @@
 # -*- coding: cp1252 -*-
 # AcquisitionEEGclientUDPandOSC.py
-# 1 : decodes EEG raw datas
-# 2 : computes spectral transform
-# 3 : extract theta, alpha, beta and gamma waves energy
-#     localised at 4 positions (AvG, AvD, ArG, ArD) for theta, alpha and beta waves
-#     but for the whole head with 3 different frequency ranges for gamma waves
-# 4 : write sensors' values, battery level, and normalised theta, alpha, beta, gamma values
-# 5 : sends theta, alpha, beta and gamma values within an UDP socket
-# 6 : sends normalised theta, alpha, beta and gamma values (in [0,1]) within an OSC socket
-
-# lumi�re comme speed
 
 from emokit.emotiv import Emotiv
 import socket
@@ -30,18 +20,15 @@ if __name__ == "__main__":
                'F4','AF4','FC6','F8',
                'T7','P7','O1',
                'T8','P8','O2'] # list of EEG electrods names
-    #EEG_FREQ = 1          # fr�quense d'acquisition de l'EEG
-    MINQ = 0 # qualit� minimale du signal exig�e (1 c'est bien, avec 0 tout passe m�me si le casque n'est pas sur la t�te)
+    MINQ = 0 # qualité minimale du signal exigée (1 c'est bien, avec 0 tout passe même si le casque n'est pas sur la tête)
     # ouverture de la socket UDP
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     UDP_IP = "127.0.0.1"    # adresse IP de la machine avec mer unity
     UDP_PORT = 5000         # port udp entre python et unity
 
-    # choix de la fr�quence des envois udp
-    udp_freq = 128           # (Hz) fr�quence de l'envoi des trames udp et osc
+    # choix de la fréquence des envois udp
+    udp_freq = 128           # (Hz) fréquence de l'envoi des trames udp et osc
     udp_period = 1./udp_freq
-    # initialisation des donn�es
-        # trames
 
     inittime=time.clock()
     lasttime=inittime
@@ -50,7 +37,7 @@ if __name__ == "__main__":
     try:
         while True:
             packet = headset.dequeue()
-            # maj du temps r�el pour d�cider de l'envoi par udp
+            # maj du temps réel pour décider de l'envoi par udp
             now=time.clock()
             if now-lasttime > udp_period :
                 # Envoi UDP des tensions d'électrode
@@ -63,7 +50,6 @@ if __name__ == "__main__":
                 sock.sendto(mystr,(UDP_IP,UDP_PORT))
 
                 # Sortie console des infos batterie
-                    #print "\rBattery", packet.battery, '% : '
                 sys.stdout.write("\rBattery : " + str(packet.battery) + '%. ')
                 if packet.battery<10 :
                     sys.stdout.write("Maybe " + str((packet.battery*600)/100) + " minutes left (LOW BATTERY).             ")
@@ -76,4 +62,3 @@ if __name__ == "__main__":
     finally:
         headset.close()
         sock.close()
-        #client.close()

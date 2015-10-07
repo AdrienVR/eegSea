de2b0e3727467fe346d0eb9c5ddeedc75544d0eb
AdrienVR <a1vernot@enib.fr>
2015-10-07 18:50:22 +0200

    TODO : resize correctly height of signal function of height of camera + add other curve to graph

---
+++ merUnity/Assets/GrahpTester.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrahpTester : MonoBehaviour 
+{
+	public float[] sensors;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (sensors.Length > 1)
+			sensors [0] = Mathf.Cos (Time.time * sensors [1]) * 50;
+
+		UpdateCurveCounts();
+		for(int i = 0;i < sensors.Length ; i++)
+		{
+			//Debug.Log(sensors[i]);
+			GraphManager.Instance.SetCurveValue(i, sensors[i]);
+		}
+	}
+
+	private void UpdateCurveCounts()
+	{
+		if (m_currentLength != sensors.Length) 
+		{
+			m_currentLength = sensors.Length;
+			if (m_currentLength > 0)
+				GraphManager.Instance.CreateNCurve (m_currentLength);
+		}
+	}
+
+	private int m_currentLength = -1;
+}
--- merUnity/Assets/MerMiroir/scripts/Curve.cs
+++ merUnity/Assets/MerMiroir/scripts/Curve.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Curve : MonoBehaviour
 {
 
-    public RectTransform TrailObjectTransform;
-    public Text ObjectValue;
+    public TrailRenderer TrailRenderer;
 
-    public float MaxValueDuration = 1;
+	public Text Value;
+	public Text GraphName;
+
+	public RectTransform TrailedPixel;
 
     // Use this for initialization
     void Start()
@@ -18,30 +21,102 @@ public class Curve : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // MAX
         if (m_currentMaxValue == null || m_value > m_currentMaxValue.Value)
         {
-            m_currentMaxValue = new MaxValue(m_value, MaxValueDuration);
-            m_currentMaxValue.Update(Time.deltaTime);
+            m_currentMaxValue = new MinMaxValue(m_value, m_valueDuration);
         }
+
+        m_maxValue = m_currentMaxValue.Value;
+        m_currentMaxValue.Update(Time.deltaTime);
+
+        if (m_currentMaxValue == null)
+            m_currentMaxValue = new MinMaxValue(LookForMaxValue(), m_valueDuration);
+
+        // MIN
+        if (m_currentMinValue == null || m_value < m_currentMinValue.Value)
+        {
+            if (m_currentMaxValue.Value != m_value)
+                m_currentMinValue = new MinMaxValue(m_value, m_valueDuration);
+            else
+                m_currentMinValue = new MinMaxValue(0, m_valueDuration);
+        }
+
+        m_minValue = m_currentMinValue.Value;
+        m_currentMinValue.Update(Time.deltaTime);
+
+        if (m_currentMinValue == null)
+            m_currentMinValue = new MinMaxValue(LookForMinValue(), m_valueDuration);
+    }
+	
+	void LateUpdate()
+	{
+		if (TrailRenderer.enabled == false)
+			TrailRenderer.enabled = true;
+	}
+
+    public void SetValue(float value)
+    {
+		m_value = value;
+        Value.text = value.ToString();
+        m_values.Add(value);
     }
 
-    public void UpdateValue(float value)
+    public void UpdatePositions()
     {
-        ObjectValue.text = value.ToString();
+		Debug.Log (m_value);
+		if (TrailedPixel != null)
+			TrailedPixel.localPosition = new Vector3 (TrailedPixel.localPosition.x + 0, 
+			                                          m_value,//TrailedPixel.localPosition.y + m_value,
+			                                          TrailedPixel.localPosition.z + 0);
+		// * m_value;// + Vector3.right * 248;//(m_value - m_minValue)/(m_maxValue);
+    }
+
+    private float LookForMaxValue()
+    {
+        float max = 0;
+        foreach(float value in m_values)
+        {
+            if (value != m_maxValue && value>max)
+            {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    private float LookForMinValue()
+    {
+        float min = 0;
+        foreach (float value in m_values)
+        {
+            if (value != m_minValue && value < min)
+            {
+                min = value;
+            }
+        }
+        return min;
     }
 
     private float m_value;
     private float m_maxValueEver;
 
-    private TrailRenderer m_trailRenderer;
+    private MinMaxValue m_currentMaxValue;
 
-    private MaxValue m_currentMaxValue;
+    private float m_valueDuration = 1;
 
-    public class MaxValue : Object
+    private float m_minValue;
+    private float m_maxValue;
+
+    private MinMaxValue m_currentMinValue;
+
+    private SlidingBuffer<float> m_values = new SlidingBuffer<float>(10);
+
+    public class MinMaxValue : Object
     {
         public float Value;
 
-        public MaxValue(float value, float lifeTime)
+        public MinMaxValue(float value, float lifeTime)
         {
             Value = value;
             m_lifeTime = lifeTime;
@@ -55,5 +130,34 @@ public class Curve : MonoBehaviour
         }
 
         private float m_lifeTime;
+    }
+
+    class SlidingBuffer<T> : IEnumerable<T>
+    {
+        private readonly Queue<T> _queue;
+        private readonly int _maxCount;
+
+        public SlidingBuffer(int maxCount)
+        {
+            _maxCount = maxCount;
+            _queue = new Queue<T>(maxCount);
+        }
+
+        public void Add(T item)
+        {
+            if (_queue.Count == _maxCount)
+                _queue.Dequeue();
+            _queue.Enqueue(item);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _queue.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }

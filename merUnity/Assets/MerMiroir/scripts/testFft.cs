1970c78e7061b0d1f3639d9e381df2a8e9e894ab
soufyaneJanati <s2janati@enib.fr>
2015-10-14 16:12:56 +0200

    ajout du fichier fft

---
+++ merUnity/Assets/MerMiroir/scripts/testFft.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testFft : MonoBehaviour {
+    private Fft fft;
+    public double entree;
+	// Use this for initialization
+	void Start () {
+	this.fft=new Fft(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            fft.update(entree);
+            for (int i = 0; i < 16;i++ )
+                Debug.Log(fft.output[i]);
+        }
+	
+	}
+}

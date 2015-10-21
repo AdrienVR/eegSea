6ad123883ee1491b2e3d7eb33cc722c5c39a0c36
soufyaneJanati <s2janati@enib.fr>
2015-10-21 15:11:06 +0200

    Revert "bug fix"
    This reverts commit 44f145497ea2f1eae186ec1987149e7d33656190.

--- merUnity/Assets/MerMiroir/scripts/ClientBehavior.cs
+++ merUnity/Assets/MerMiroir/scripts/ClientBehavior.cs
@@ -14,7 +14,13 @@ public class ClientBehavior : MonoBehaviour {
 
     public int numberValuesReceived = 0;
 
+<<<<<<< HEAD
+    static string[] sensorName = new string[14];
+    static float[] sensorVal = new float[14];
+    static float sensorTime = 0.0f;
+=======
     public const bool UseCoroutine = false;
+>>>>>>> interfaceGraph
 
     void Awake()
     {
@@ -87,10 +93,56 @@ public class ClientBehavior : MonoBehaviour {
     {
         if (UseCoroutine)
         {
+<<<<<<< HEAD
+            try
+            {
+                IPEndPoint receiver = new IPEndPoint(IPAddress.Any, 0); //spécification du port et de l'adresse IP a écouter
+                byte[] receivedBytes = client.Receive(ref receiver); //récéption de la trame UDP
+                data1 = Encoding.ASCII.GetString(receivedBytes); //transformer la trame en chaine
+
+                //Réception des données de capteur brutes
+        				//Format : 4.391409;e1:0;e2:0;e3:0;e4:0;2:-0.146730474455;e3:-0.146730474455;e4:-0.146730474455;74;
+        				// temps;capteur1:val1;capteur2:val2;capteur3:val3;
+        				string[] debuf = data1.Split(new char[] { '|' });
+
+                for(int j=0 ; j<debuf.Length ; j++)
+                {
+                  string[] sensorStrings = debuf[j].Split(new char[] { ';' });;
+                  bool first = true;
+          				for(int i=0 ; i<sensorStrings.Length-1 ; i++)
+          				{
+          					//Information de temps en première position
+          					if(first)
+          					{
+                      sensorTime = float.Parse(sensorStrings[0], CultureInfo.InvariantCulture.NumberFormat);
+          						first = false;
+                    }
+          					//Noms et valeurs de capteurs ensuite
+          					else
+          					{
+          						string[] sensorValues = sensorStrings[i].Split(new char[] { ':' });
+          						sensorName[i-1] = sensorValues[0];
+          						sensorVal[i-1] = float.Parse(sensorValues[1], CultureInfo.InvariantCulture.NumberFormat);
+                      //Debug.Log(sensorValues[0] + " - ");
+                      //Debug.Log(sensorValues[1] + "\n");
+          						Debug.Log(sensorTime.ToString() + " - " + sensorName[i-1] + " : " + sensorVal[i-1].ToString() + "\n");
+          					}
+          				}
+                  numberValuesReceived += 1;
+                }
+
+
+
+                /*if (data.Length > 5)
+                {
+                    //Debug.Log (data.ToString());
+                    string[] split = data.Split(new char[] { ';' });
+=======
             return Time.realtimeSinceStartup;
         }
         return DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond / 1000f;
     }
+>>>>>>> interfaceGraph
 
     private void ReceiveData()
     {
@@ -100,16 +152,31 @@ public class ClientBehavior : MonoBehaviour {
             byte[] receivedBytes = client.Receive(ref receiver); //récéption de la trame UDP
             data = Encoding.ASCII.GetString(receivedBytes); //transformer la trame en chaine 
 
+<<<<<<< HEAD
+
+                }*/
+                //lastDataReceived = data;
+
+                //lastTimestampReceived = Time.realtimeSinceStartup;
+            }
+            catch (Exception Err)
+=======
             if (data.Length > 5)
+>>>>>>> interfaceGraph
             {
                 //Debug.Log (data.ToString());
                 string[] split = data.Split(new char[] { ';' });
 
                 EEGDataManager.Instance.Update(split);
 
+<<<<<<< HEAD
+               // if (((Time.realtimeSinceStartup) - lastTimestampReceived) > 1)
+				//   EEGData.Instance.ResetValues();
+=======
                 lastDataReceived = data;
                 numberValuesReceived += 1;
                 lastTimestampReceived = GetRealTime();
+>>>>>>> interfaceGraph
             }
         }
         catch (Exception Err)
@@ -138,7 +205,7 @@ public class ClientBehavior : MonoBehaviour {
             ReceiveData();
             yield return new WaitForSeconds(.05f);
 		}
-		
+
 		yield break;
     }
 
@@ -146,6 +213,7 @@ public class ClientBehavior : MonoBehaviour {
 
     // Use this for initialization
     private string data = "";
+    private string data1 = "";
     private UdpClient client;
     private int port = 5000;
     private byte[] receivedBytes;
